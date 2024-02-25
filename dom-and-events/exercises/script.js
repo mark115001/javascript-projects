eb7b5874940a68e2a4059a08389d53399e4ab85c
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-02-25 14:41:00 -0600

    Class 4 Exercises: DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,16 @@
 function init () {
     const missionAbort = document.getElementById("abortMission");
+    missionAbort.addEventListener("click", (event) => {
+        if (confirm("Are you sure you want to abort the mission")) {
+            missionAbort.innerHTML = "Mission aborted! Space shuttle returning home"
+        }
+    })
     const button = document.getElementById("liftoffButton");
+    button.addEventListener("click", (event) => button.innerHTML = "Houston, we have liftoff")
+
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+}   
 
 window.addEventListener("load", init);
