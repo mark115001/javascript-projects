315ad93b75c230f6ae1f31faa67a2f70f2f574fb
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-02-29 11:57:44 -0600

    Class 15 Exercises: Forms

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,14 @@
 //Code Your Solution Below
+function init() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit"), () => {
+        alert("submit clicked")
+    }
+}
+
+
+
+
+
+
+window.addEventListener("load", init());
