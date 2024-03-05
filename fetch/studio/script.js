128121f70a9fe5ce9f4d0a1ff2284985a51ffd44
mark115001 <145294877+mark115001@users.noreply.github.com>
2024-03-05 08:09:36 -0600

    Updated Working correctly

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -6,21 +6,18 @@ window.addEventListener("load", function() {
         console.log(json)
         
         const container = document.getElementById("container");
-        let newTag = document.createElement('div');
-        newTag.className = "astronaut";
-        newTag.innerHTML = "Hello World from innerHTML";
-        container.appendChild(newTag);
         for (i=0; i<7; i++) {
-        container.innerHTML += `<div class="bio"></div>`;
-        container.innerHTML += `<h3> ${json[i].firstName} ${json[i].lastName}</h3>`
-        container.innerHTML += `<ul>
+        container.innerHTML += `<div class="astronaut">
+        <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+        <ul>
         <li>Hours in Space: ${json[i].hoursInSpace}</li>
         <li>Active: ${json[i].active}</li>
         <li>Skills: ${json[i].skills}</li>
-        </ul>`
-        newTag = document.createElement('img')
-        newTag.className = "avatar"
-        container.appendChild(newTag)
+        </ul>
+        </div>
+        <img class="avatar" src=${json[i].picture}>
+        </div>`
         };
        });
 
